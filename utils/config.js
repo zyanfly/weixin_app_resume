c87d21cdbebaf0780a17bb9ad3a4dc97930b483a
jayzen <jayzen@foxmail.com>
2019-05-26 10:16:03 +0800

    do some fix

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,7 @@
 const config = {
-    //api_base_url: 'https://zhengjiajun.com/api/v1/',
-    api_base_url: 'http://localhost:3000/api/v1/',
-    appkey: "175lki7w4x",
+    api_base_url: 'https://zhengjiajun.com/api/v1/',
+    //api_base_url: 'http://localhost:3000/api/v1/'
+    appkey: "ew7ioq7y3q",
 }
 
 export { config }
